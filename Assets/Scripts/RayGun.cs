2ac5ff5f4edf85fcd568c7b95172158c5473dd9f
unknown <eldonlin24@gmail.com>
2024-12-21 22:19:01 -0800

    add raygun that shoots at walls. The raypatch will detect walls and stay there and gets destroyed after a few seconds

---
+++ Assets/Scripts/RayGun.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayGun : MonoBehaviour
+{
+    public LayerMask layerMask; 
+    public OVRInput.RawButton shootingButton;
+    public LineRenderer linePrefab;
+    public GameObject rayImpactPrefab;
+    public Transform shootingPoint;
+    public float maxLineDistance = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(shootingButton))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        Debug.Log("pew pew");
+
+        Ray ray = new Ray(shootingPoint.position,shootingPoint.forward);
+        bool hasHit = Physics.Raycast(ray, out RaycastHit hit, maxLineDistance, layerMask);
+        Vector3 endPoint = Vector3.zero;
+        if(hasHit)
+        {
+            endPoint = hit.point;
+            Quaternion rayImpactRotation = Quaternion.LookRotation(-hit.normal);
+            GameObject rayImpact = Instantiate(rayImpactPrefab, hit.point,rayImpactRotation);
+            Destroy(rayImpact, 2.0f);
+        }
+        else
+        {
+            endPoint = shootingPoint.position + shootingPoint.forward * maxLineDistance;
+        }
+
+        LineRenderer line = Instantiate(linePrefab);
+        line.positionCount = 2;
+        line.SetPosition(0,shootingPoint.position);
+
+;
+        line.SetPosition(1,endPoint);
+        Destroy(linePrefab.gameObject, 1.0f);
+    }
+}
