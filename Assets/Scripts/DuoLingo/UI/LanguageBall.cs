0e8d2e632ba7dfc120d59e3f0955c47546a070ba
unknown <eldonlin24@gmail.com>
2025-01-08 00:16:21 -0800

    Allow user to select a ball with duo and when user lets go the ball goes back to duo along with the beak

--- Assets/Scripts/DuoLingo/UI/LanguageBall.cs
+++ Assets/Scripts/DuoLingo/UI/LanguageBall.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class LanguageBall : MonoBehaviour
 {
-    public Material outlineMaterial; 
+    public Material outlineMaterial;
     public Material regualrMaterial;
 
     public MeshRenderer meshRenderer;
 
     public Transform startTransform;
-    public Transform endTransform; 
-    
+    public Transform endTransform;
+
+    public bool isBallTriggerEnter = false;
+
     public void DisplayOutLineMaterial()
     {
         meshRenderer.material = outlineMaterial;
@@ -27,6 +29,7 @@ public class LanguageBall : MonoBehaviour
         Debug.Log("collision entered ");
         if (other.gameObject.tag == "Beak")
         {
+            isBallTriggerEnter = true;
             Debug.Log($"collision entered languageball {other.gameObject.name}");
             DisplayOutLineMaterial();
         }
@@ -36,7 +39,9 @@ public class LanguageBall : MonoBehaviour
     {
         if (other.gameObject.tag == "Beak")
         {
+            isBallTriggerEnter = false;
             DisplayRegularMaterial();
         }
     }
+
 }
--- Assets/Scripts/DuoLingo/UI/LanguageBallController.cs
+++ Assets/Scripts/DuoLingo/UI/LanguageBallController.cs
@@ -5,34 +5,55 @@ using DG.Tweening;
 public class LanguageBallController : MonoBehaviour
 {
     public List<LanguageBall> languageBalls = new();
+    public Transform breakTransform;
     // Start is called before the first frame update
     void Start()
     {
-        foreach(var languageBall in languageBalls)
+        foreach (var languageBall in languageBalls)
         {
             languageBall.gameObject.SetActive(false);
-        }      
+        }
     }
 
     public void DisplayBall()
     {
 
-        foreach(var languageball in languageBalls)
+        foreach (var languageball in languageBalls)
         {
             languageball.gameObject.SetActive(true);
-            languageball.transform.DOMove(languageball.endTransform.position,1.0f);
+            languageball.transform.DOMove(languageball.endTransform.position, 1.0f);
         }
     }
 
     public void HideBall()
-    {       
+    {
 
-        foreach(var languageball in languageBalls)
+        foreach (var languageball in languageBalls)
         {
-            languageball.transform.DOMove(languageball.startTransform.position, 1.0f).OnComplete(() =>
+            if (!languageball.isBallTriggerEnter)
             {
-                languageball.gameObject.SetActive(false);
-            });
+                languageball.transform.DOMove(languageball.startTransform.position, 1.0f).OnComplete(() =>
+                {
+                    languageball.gameObject.SetActive(false);
+                });
+
+            }
+
+        }
+    }
+
+    public void CheckSelectedBall()
+    {
+        foreach (var languageball in languageBalls)
+        {
+            if (!languageball.isBallTriggerEnter)
+            {
+                continue;
+            }
+            if (languageball.isBallTriggerEnter)
+            {
+                languageball.transform.DOMove(breakTransform.position, 0.5f);
+            }
 
         }
     }
