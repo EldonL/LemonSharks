37c9b9aedf1406eb65a29a45304a797a37c0c66b
unknown <eldonlin24@gmail.com>
2025-01-08 22:54:06 -0800

    Allow duo fly perch to land on finger but not a child and when user does not do point anymore, owl plays an animation

--- Assets/Scripts/DuoLingo/DuoOwl/DuoFlying.cs
+++ Assets/Scripts/DuoLingo/DuoOwl/DuoFlying.cs
@@ -7,22 +7,27 @@ public class DuoFlying : MonoBehaviour
     public GameObject objectToMove;
     public GameObject leftHand;
     public GameObject rightHand;
+
+    [SerializeField] private Animator DuoAnimator;
     public void FlyTowardsLeftHand()
     {
         objectToMove.transform.DOMove(leftHand.transform.position,1).OnComplete(()=>{
-            objectToMove.transform.SetParent(leftHand.transform);
+           // objectToMove.transform.SetParent(leftHand.transform);
         });
     }
 
     public void FlyTowardsRightHand()
     {
         objectToMove.transform.DOMove(rightHand.transform.position,1).OnComplete(()=>{
-            objectToMove.transform.SetParent(rightHand.transform);
+            //objectToMove.transform.SetParent(rightHand.transform);
         });
     }
 
     public void UnSelectedHand()
     {
-        objectToMove.transform.SetParent(null);
+        // objectToMove.transform.SetParent(null);
+            DuoAnimator.Play("Takeoff");
     }
+    
+
 }
