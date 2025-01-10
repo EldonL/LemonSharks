e183fba94788fcf7b9138b48f23c447f8700457b
unknown <eldonlin24@gmail.com>
2025-01-09 20:30:59 -0800

    Grab crying owl so it flips and starts crying and we shake it like a ragdoll

---
+++ Assets/Scripts/DuoLingo/DuoOwl/DuoFlip.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuoFlip : MonoBehaviour
+{
+    
+    [SerializeField] private Animator DuoAnimator;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Grabbed();
+        }
+    }
+    public void Grabbed()
+    {            
+        Debug.Log($"<color=yellow>gar bunkle</color>");
+        // objectToMove.transform.SetParent(null);
+            DuoAnimator.Play("FlyIdle1");
+
+    }
+}
